bd6e1485906d4a69b17dcb85bdd68c46c7a30371
Arjun Sah <arjun.sah@jumpseller.com>
2020-05-22 22:11:37 +0100

    first commit

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,42 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
-const Lists = ({posts}) => {
-  const [listItems, setListItems] = useState(posts.slice(0,20));
-  const [isFetching, setIsFetching] = useState(false);
+const Lists = () => {
+      const [page, setPage] = useState(1);
+      const [manifestList, setManifestList] = useState([]);
+      const [isFetching, setIsFetching] = useState(false);
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+      useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+      }, []);
 
-  useEffect(() => {
-    if (!isFetching) return;
-    fetchMoreListItems();
-  }, [fetchMoreListItems, isFetching]);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      function getManifests(pageNo){
+          axios.get("http://localhost:3002/posts", { params: { _page: pageNo, _limit:20 } }).then(res => {
+                console.log("data...", res.data);
+                setManifestList([...manifestList, ...res.data]);
+                setIsFetching(false);
+          });
+      }
 
-  function handleScroll() {
-    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || isFetching) return;
-    setIsFetching(true);
-  }
+      useEffect(() => {
+        getManifests(page);
+      }, []);
 
-  function fetchMoreListItems() {
-    setTimeout(() => {
-        setListItems(prevState => ([...prevState, ...posts.slice(listItems.length, (listItems.length + 20)|| "")]));
-        setIsFetching(false);
-    }, 2000);
-  }
+      useEffect(() => {
+        if (!isFetching){
+          return;
+        }
+        getManifests(page);
+        
+      }, [isFetching]);
+
+      function handleScroll() {
+        if (
+          window.innerHeight + document.documentElement.scrollTop !==
+            document.documentElement.offsetHeight ||
+          isFetching
+        )
+          return;
+          console.log("bottom hit...");
+          let pageN = page + 1;
+          setPage(pageN);
+          setIsFetching(true);
+      }
+
+  console.log("page Number..", page);
   
   return (
-    <>
+    <div>Arjun and shiva best amigos third..fifth
       <ul className="list-group mb-2">
-        {listItems.map((listItem, i) => <li key={i} className="list-group-item"> {listItem.title}</li>)}
+        {manifestList.map((listItem, i) => <li key={i} className="list-group-item"> {listItem.title}</li>)}
       </ul>
-      {isFetching && (listItems.length < posts.length) && (
+      {/* {isFetching  && (
         <div className="spinner-border" role="status">
             <span className="sr-only">Loading Loading...</span>
         </div>
-      )}
-    </>
+      )} */}
+    </div>
   );
 };
 
